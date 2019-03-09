e68edaea0840e1c433b4b8d3d1d34c95df2c9008
will <zahfoxtales@gmail.com>
2019-03-09 13:43:13 -0600

    feat: more progress with migrate command

--- src/migrate/migrate.ts
+++ src/migrate/migrate.ts
@@ -1,3 +1,5 @@
+import { stat } from 'fs-extra'
+
 import { CouchConfig } from '../config'
 
 export type migrateArgs = {
@@ -5,6 +7,40 @@ export type migrateArgs = {
   databaseName: string
 }
 
+interface MigrationScript {
+  transform: (document: any) => any
+}
+
 export async function migrate(args: migrateArgs) {
   console.log(args)
+  await ensureMigrateFileExists()
+  const { transform } = importMigrationScript()
+  transform({})
+}
+
+function importMigrationScript(): MigrationScript {
+  const migrationScript = require(`${process.cwd()}/migrate.js`)
+
+  if (typeof migrationScript !== 'function') {
+    throw new Error('The migrate.js script must export a function that returns the transform function.')
+  }
+
+  const migrationFunctions = migrationScript()
+
+  if (!migrationFunctions || typeof migrationFunctions.transform !== 'function') {
+    throw new Error('No transform function was returned from the migrate.js script')
+  }
+
+  return migrationFunctions as MigrationScript
+}
+
+async function ensureMigrateFileExists() {
+  try {
+    const result = await stat(`${process.cwd()}/migrate.js`)
+    if (!result.isFile) {
+      throw new Error('A migrate.js script is required to apply a database migration.')
+    }
+  } catch (e) {
+    throw new Error('A migrate.js script is required to apply a database migration.')
+  }
 }
