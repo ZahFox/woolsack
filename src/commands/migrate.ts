d3439b191c61b2c2024b610a44d923cd0dc1e926
will <zahfoxtales@gmail.com>
2019-03-15 01:32:22 -0500

    chore: still making the linter happy

--- src/commands/migrate.ts
+++ src/commands/migrate.ts
@@ -5,26 +5,26 @@ import { CouchConfig, Environment, getConfig, getEnv } from '../config'
 import { migrate } from '../migrate'
 
 export default class Migrate extends WoolsackCommand {
-  static description = 'use a function to update a set of documents'
+  public static description = 'use a function to update a set of documents'
 
-  static examples = [`$ woolsack migrate -d test-database`]
+  public static examples = [`$ woolsack migrate -d test-database`]
 
-  static flags = {
+  public static flags = {
     help: flags.help({ char: 'h' }),
     databaseName: flags.string({ char: 'd', description: 'the database that the migration will be applied to' })
   }
 
-  static args = []
+  public static args = []
 
-  async run() {
+  public async run() {
     try {
-      const { flags } = this.parse(Migrate)
+      const { flags: runtimeFlags } = this.parse(Migrate)
 
-      if (!isString(flags.databaseName)) {
+      if (!isString(runtimeFlags.databaseName)) {
         this.logError('A database name is required to run a migration')
       }
 
-      const databaseName = flags.databaseName as string
+      const databaseName = runtimeFlags.databaseName as string
       const env: Environment = getEnv()
       const config: CouchConfig = getConfig()
       await migrate({ config, databaseName, env })
